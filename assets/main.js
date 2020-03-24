aac4b7e8c7d941311273b4afccde03248d0dce20
Blandon Tang <blandon.tang@live.ca>
2020-03-24 11:37:51 -0600

    initial commit

---
+++ assets/main.js
@@ -0,0 +1,134 @@
+// Blandon Tang
+// 10126829 W2020 SENG513
+// Client Side Code
+/**
+ * https://socket.io/docs/client-api/#socket-id -- socket io documentation
+ * https://socket.io/docs/emit-cheatsheet/  -- socket io cheatsheet
+ * https://stackabuse.com/git-merge-branch-into-master/ -- git branching cheatsheet
+ */
+
+const socket = io();
+var $loginForm = $('#login-form');
+var $loginArea = $('#login-area');
+var $msgForm = $('#messageForm');
+var $messageArea = $('#messagesArea');
+let $username;
+var usersOnline = {}; // equivalent to connectedUsers in serverside
+
+socket.on('connect', function() {
+	// check if cookie is present
+	let fillerName = 'newUserName';
+	let hasCookie = false;
+	if (document.cookie.split(';').filter((item) => item.trim().startsWith('username=')).length) {
+		// console.log('The cookie "username" exists')
+		fillerName = document.cookie.replace(/(?:(?:^|.*;\s*)username\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+		// console.log(fillerName);
+		hasCookie = true;
+	}
+    socket.emit('newUser', {
+	    username: fillerName,
+		room: 'chatroom',
+		color: '#000000', // default nickname color is black
+		cookie: hasCookie
+	});
+});
+
+socket.on('message', function(message) {
+	let momentTimestamp = moment.utc(message.timestamp);
+	// console.log("on message: " + socket.id);
+	// console.log(usersOnline);
+	// console.log("username color is: " + message.color);
+	let $message = $('#messagesArea');
+
+	// If Message is a system message
+	if ((message.userID === undefined) &&(message.username.toLowerCase() == 'system')){
+		$message.append('<div class="sysMsg"><div><span>' + message.username + ' </span><span>' + momentTimestamp.local().format('h:mma') +'</span></div>' +
+						'<div>' + message.text + '</div></div>');
+	}
+	// Message is sent by own client 
+	else if (message.userID == socket.id){
+		$message.append('<div class="msg right-msg"><div class="msgBubble">' +
+							'<div class="msgInfo">' +
+								'<div class="msgInfo-name" style="color: '+ message.color +' !important;">' + message.username + '</div>' +
+								'<div class="msgInfo-time">' + momentTimestamp.local().format('h:mma') + '</div>' +
+							'</div>' +
+							'<div class="msg-text">' +
+								message.text +
+							'</div>' +
+						'</div></div>');
+	}
+	// If message comes from other user
+	else {
+		$message.append('<div class="msg left-msg"><div class="msgBubble">' +
+							'<div class="msgInfo">' +
+								'<div class="msgInfo-name" style="color: '+ message.color +' !important;">' + message.username + '</div>' +
+								'<div class="msgInfo-time">' + momentTimestamp.local().format('h:mma') + '</div>' +
+							'</div>' +
+							'<div class="msg-text">' +
+								message.text +
+							'</div>' +
+						'</div></div>');
+	}
+	scrollToBottom('messagesArea');
+});
+
+socket.on('showChatLog', function(chatHistory){
+	// console.log(chatHistory);
+	var $message = $('#messagesArea');
+	chatHistory.forEach(function(message){
+		var momentTimestamp = moment.utc(message.time);
+		$message.append('<div class="msg left-msg"><div class="msgBubble">' +
+		'<div class="msgInfo">' +
+			'<div class="msgInfo-name">' + message.user + '</div>' +
+			'<div class="msgInfo-time">' + momentTimestamp.local().format('h:mma') + '</div>' +
+		'</div>' +
+		'<div class="msg-text">' +
+			message.msg +
+		'</div>' +
+	'</div></div>');
+	});
+	scrollToBottom('messagesArea');
+})
+
+socket.on('usersPresent', function(connectedUsers){
+	usersOnline = connectedUsers;
+	$username = usersOnline[socket.id].username;
+	document.cookie = "username=" + $username; //set cookie in case of nickname change
+	// console.log(document.cookie);
+	$(".roomTitle").text('Welcome to the chatroom, ' + $username + '!'); // change name display at top
+
+	let allUsers = "";
+	Object.keys(connectedUsers).forEach(function(socketID){
+		// console.log(connectedUsers[socketID].username); // get all usernames present in chatroom
+		allUsers += "<div class='userDisplay'><span style='color: "+ connectedUsers[socketID].color +";'><strong>" + connectedUsers[socketID].username + "</strong></span></div>"; // change color of name in online list as well
+		// console.log(allUsers);
+	});
+	$('#usersContainer').html(allUsers); // display all users
+})
+
+$msgForm.on('submit', function(e) {
+	e.preventDefault(); // prevent default form reload
+	let $message = $('#messageInput');
+	let invalidInput = /<(.|\n)*?>/g;
+	if (invalidInput.test($message.val())) {
+		alert('html tags are not allowed');
+    }
+    else {
+		// console.log(socket.id);
+		// console.log(usersOnline[socket.id].username);
+		socket.emit('message', {
+			username: $username.trim(),
+			text: $message.val(),
+			color: usersOnline[socket.id].color,
+			userID: socket.id
+		});
+	}
+	$message.val('');
+});
+
+function scrollToBottom(id) {
+	let section = document.getElementById(id);
+	$('#' + id).animate({
+		scrollTop: section.scrollHeight - section.clientHeight
+	}, 450);
+}
---
+++ index.js
@@ -0,0 +1,191 @@
+// Blandon Tang
+// 10126829 W2020 SENG513
+// Server side code
+/**
+ * 
+ */
+
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var moment = require('moment');
+var chance = require('chance').Chance();
+
+var connectedUsers = {}; // object of all connected user objects
+var chatHistory = [];
+
+app.use(express.static('assets'));
+
+// Whenever user makes get request to server
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+	console.log('A user has connected with ID: ' + socket.id);
+	// console.log(connectedUsers);
+	// io.emit('usersPresent', connectedUsers);
+	
+	socket.on('disconnect', function() {
+		var userData = connectedUsers[socket.id];
+		if (typeof userData !== 'undefined') { // if element does not exist
+			socket.leave(connectedUsers[socket.id]);
+			io.to(userData.room).emit('message', {
+				username: 'System',
+				text: userData.username + ' has left the chat',
+				timestamp: moment().valueOf(),
+				color: '#808080'
+			});
+			delete connectedUsers[socket.id];
+			io.emit('usersPresent', connectedUsers); // write clientside userlist
+			// console.log(connectedUsers);
+		}
+	});
+
+	// socket.emit('displayChatLog', {chatHistory});
+
+	socket.on('newUser', function(req, callback) {
+		let nameTaken = false;
+		// req.username = chance.animal();
+		if (req.cookie == false){
+			req.username = chance.animal(); // generate new name if no cookie
+		}
+        // connectedUsers.push(socket.id);
+        // connectedUsers[socket.id]['username'] = newUserName;
+        // console.log(connectedUsers);
+		
+		// generate another name if taken
+		Object.keys(connectedUsers).forEach(function(socketID) {
+			while (connectedUsers[socketID].username.toLowerCase() === req.username.toLowerCase()) {
+				req.username = chance.animal();
+				console.log("name taken, trying new name: " + req.username);
+            }
+		});
+		
+		console.log(req.username + " has joined the chatroom");
+		connectedUsers[socket.id] = req;
+		connectedUsers[socket.id].userID = socket.id;
+		socket.join(req.room);
+		// console.log(connectedUsers);
+		// console.log(chatHistory);
+
+		io.emit('usersPresent', connectedUsers); // send a data struct of all current users to client
+		io.to(`${socket.id}`).emit('showChatLog', chatHistory); // emit only to new joinee
+
+		socket.broadcast.to(req.room).emit('message', {
+			username: 'System',
+			text: req.username + ' has joined!',
+			timestamp: moment().valueOf(),
+			color: '#808080'
+		});
+	});
+
+	
+	socket.emit('message', {
+		username: 'System',
+		text: 'Stay awhile and listen <br><i>/nick</i> - change your nickname<br><i>/nickcolor</i> - change nickname color',
+		timestamp: moment().valueOf(),
+		color: '#808080'
+	});
+
+	socket.on('message', function(message) {
+		message.timestamp = moment().valueOf();
+		io.to(connectedUsers[socket.id].room).emit('message', message);
+		chatHistory.push({user:message.username, msg:message.text, time:message.timestamp});
+		// console.log(chatHistory);
+
+		// User has indicated a nickname change
+		if ((message.text.charAt(0) ==='/') && (message.text.indexOf("nick ") == 1)){
+			// console.log("change the nickname");
+			let userInput = message.text.split(' ');
+			if ((userInput.length != 2) || (userInput[1].length < 1) || (userInput[1].charAt(0) == ' ')) {
+				socket.emit('message', {
+					username: 'System',
+					text: 'Invalid format, please use: /nick new_nickname',
+					timestamp: moment().valueOf(),
+					color: '#808080'
+				});
+			}
+			else if (userInput[1].toLowerCase() === 'system'){
+				socket.emit('message', {
+					username: 'System',
+					text: 'Sorry, that name cannot be selected',
+					timestamp: moment().valueOf(),
+					color: '#808080'
+				});
+			}
+			else{
+				let newName = userInput[1];
+				let nameInUse = false;
+				Object.keys(connectedUsers).forEach(function(socketID) {
+					if (connectedUsers[socketID].username.toLowerCase() === newName.toLowerCase()) {
+						nameInUse = true;
+					}
+				});
+				if (nameInUse == false){
+					connectedUsers[socket.id].username = newName;
+					console.log("Name changed to: " + newName);
+					io.emit('usersPresent', connectedUsers);
+					// console.log(connectedUsers);
+				}
+				else{
+					socket.emit('message', {
+						username: 'System',
+						text: 'Sorry, the selected nickname is taken',
+						timestamp: moment().valueOf(),
+						color: '#808080'
+					});
+				}
+			}
+		}
+
+		// User has indicated a nickname color change
+		else if ((message.text.charAt(0) ==='/') && (message.text.indexOf("nickcolor ") == 1)){
+			let userInput = message.text.split(' ');
+			if ((userInput.length != 2) || (userInput[1].length != 6)){
+				socket.emit('message', {
+					username: 'System',
+					text: 'Invalid format, please use: /nickcolor RRGGBB',
+					timestamp: moment().valueOf(),
+					color: '#808080'
+				});
+			}
+			else{
+				let rgbValue = userInput[1];
+				rgbValue  = "#" + rgbValue;
+				// True if valid hex code
+				if (/^#[0-9A-F]{6}$/i.test(rgbValue)){
+					// console.log('valid RGB value: ' + rgbValue);
+					connectedUsers[socket.id].color = rgbValue;
+					io.emit('usersPresent', connectedUsers);
+					// console.log(connectedUsers);
+				}
+				else{
+					socket.emit('message', {
+						username: 'System',
+						text: 'Please enter a valid hex color code',
+						timestamp: moment().valueOf(),
+						color: '#808080'
+					});
+				}
+				
+			}
+		}
+
+		// Invalid slash command
+		else if (message.text.charAt(0) ==='/'){
+			// console.log("Invalid / command");
+			socket.emit('message', {
+				username: 'System',
+				text: 'Invalid slash command or invalid format',
+				timestamp: moment().valueOf(),
+				color: '#808080'
+			});
+		}
+
+	});
+});
+
+http.listen(3030);
+// http.listen(process.env.PORT || 3000);
